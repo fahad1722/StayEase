b2cc72f359b0acaba7f1cc2fa9d4b1e4be63a581
fahad1722 <mohammedfahad1905@gmail.com>
2026-04-19 14:33:41 +0530

    Removed Auth for Ping Controller

--- src/main/java/com/fahad/stayease/config/SecurityConfig.java
+++ src/main/java/com/fahad/stayease/config/SecurityConfig.java
@@ -63,7 +63,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/actuator/health",
-                                "/actuator/info"
+                                "/actuator/info",
+                                "/ping"
                         ).permitAll()
                         .requestMatchers("/actuator/**").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/register", "/api/v1/auth/login", "/api/v1/auth/refresh-token").permitAll()
